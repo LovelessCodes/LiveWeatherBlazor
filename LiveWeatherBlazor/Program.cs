32ba1410cfe5474e0b05f53f200db3c42da4ebd8
LovelessCodes <hello@loveless.codes>
2022-11-03 13:02:48 +0100

    Fixed some css errors

--- LiveWeatherBlazor/Program.cs
+++ LiveWeatherBlazor/Program.cs
@@ -1,6 +1,6 @@
+using LiveWeatherBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Test123_Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
